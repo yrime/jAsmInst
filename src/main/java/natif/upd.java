f1e5b049b837880f6b3c1d8d9fcdb55ca20838e0
yrime <yrime@mail.ru>
2022-03-22 17:16:29 +0300

    Update upd.java

--- src/main/java/natif/upd.java
+++ src/main/java/natif/upd.java
@@ -9,15 +9,15 @@ public class upd {
 
     public static void __afl_maybe_log(int i){
         try {
-            BufferedWriter writer = new BufferedWriter(new FileWriter("javalog.txt", true));
+        //    BufferedWriter writer = new BufferedWriter(new FileWriter("javalog.txt", true));
 
-            writer.write("null ");
+        //    writer.write("null ");
 
         //    System.out.println("natif " + i + " ");
             //  bb.put(i, (byte) 1);
-            writer.write("i: " + i + "\n");
+         //   writer.write("i: " + i + "\n");
             // writer.write(bb.array().toString());
-            writer.close();
+         //   writer.close();
             natifUpd.mapUp(i);
         } catch (IOException e) {
             e.printStackTrace();
