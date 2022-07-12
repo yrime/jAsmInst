5cf96dffbaa54c34836c83f7b8fbd4b230fbcdcd
yrime <yrime@mail.ru>
2022-07-12 17:41:39 +0300

    Update instrumentation.bat

--- instrumentation.bat
+++ instrumentation.bat
@@ -1,3 +1,9 @@
 COPY /B /Y %1%2 ModifyClasses
 
-java -jar target\jAsmInst-1.0-jar-with-dependencies.jar %1%2 %3 cd ModifyClasses && jar uf %2 * cd ../
+java -jar target\jAsmInst-1.0-jar-with-dependencies.jar %1%2 %3 
+
+cd ModifyClasses
+
+jar uf %2 * 
+
+cd ../
