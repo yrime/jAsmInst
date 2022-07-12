8fb0ab5083d12893913edcf3f291b6d446f8c965
yrime <yrime@mail.ru>
2022-07-12 17:42:02 +0300

    Update upd.java

--- src/main/java/natif/upd.java
+++ src/main/java/natif/upd.java
@@ -8,19 +8,8 @@ import java.nio.ByteBuffer;
 public class upd {
 
     public static void __afl_maybe_log(int i){
-        try {
-        //    BufferedWriter writer = new BufferedWriter(new FileWriter("javalog.txt", true));
-
-        //    writer.write("null ");
-
-        //    System.out.println("natif " + i + " ");
-            //  bb.put(i, (byte) 1);
-         //   writer.write("i: " + i + "\n");
-            // writer.write(bb.array().toString());
-         //   writer.close();
+       
             natifUpd.mapUp(i);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        
     }
 }
